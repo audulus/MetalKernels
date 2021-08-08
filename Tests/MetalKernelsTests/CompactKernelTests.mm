8e45d7c7ef2f978bb760c8eb34c2d542318ea62d
Taylor Holliday <taylor@audulus.com>
2021-08-08 12:05:20 -0700

    Update CompactKernelTests.mm

--- Tests/MetalKernelsTests/CompactKernelTests.mm
+++ Tests/MetalKernelsTests/CompactKernelTests.mm
@@ -55,4 +55,41 @@
     }
 }
 
+// Many iOS devices have a max buffer size of 256MB.
+#define MAX_BUFFER_SIZE (256*1024*1024)
+
+- (void) testCompact2 {
+
+    int n = MAX_BUFFER_SIZE/sizeof(uint);
+
+    kernel.maxLength = n;
+
+    std::vector<uint> vec(n);
+    for(int i=0;i<n;++i)
+    {
+        vec[i] = i;
+    }
+
+    auto inBuf = [device newBufferWithBytes:vec.data() length:vec.size()*sizeof(uint) options:MTLResourceStorageModeShared];
+
+    auto outBuf = [device newBufferWithLength:vec.size()*sizeof(uint) options:MTLResourceStorageModeShared];
+
+    auto buf = [queue commandBuffer];
+
+    [kernel encodeCompactTo:buf input:inBuf output:outBuf itemSize:sizeof(uint) length:n];
+
+    [buf commit];
+    [buf waitUntilCompleted];
+
+    auto outPtr = (uint*) outBuf.contents;
+
+    for(int i=0;i<n;++i) {
+        auto x = outPtr[i];
+        XCTAssertTrue(x == 0 || (x & 1));
+        if(!(x==0 || (x&1))) {
+            break;
+        }
+    }
+}
+
 @end
