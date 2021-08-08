fcb6cd1ac5527fe1fe9388bdd47070a993d59584
Taylor Holliday <taylor@audulus.com>
2021-08-08 10:17:09 -0700

    Update CompactKernel.h

--- Sources/MetalKernels/include/CompactKernel.h
+++ Sources/MetalKernels/include/CompactKernel.h
@@ -4,10 +4,19 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/// Stream compaction using a user-specified predicate.
 @interface CompactKernel : NSObject
 
+/// Create a CompactKernel.
+/// @param predicate Metal function declared as [[visible]]. Must be bool functionName(device char*).
 - (instancetype)initWithPredicate:(nonnull id<MTLFunction>)predicate;
 
+/// Encodes commands to compact a buffer.
+/// @param buffer command buffer for encoding
+/// @param inputBuf input buffer of items
+/// @param outputBuf output buffer of compacted items
+/// @param itemSize size of each item in bytes
+/// @param length number of items in input buffer
 - (void) encodeCompactTo:(id<MTLCommandBuffer>)buffer
                    input:(id<MTLBuffer>)inputBuf
                   output:(id<MTLBuffer>)outputBuf
