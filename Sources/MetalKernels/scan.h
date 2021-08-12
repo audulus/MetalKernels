2deb0ac5f89c1d9218a8815cdc1d231ca98a4be1
Taylor Holliday <taylor@audulus.com>
2021-08-12 08:35:12 -0700

    Update scan.h

--- Sources/MetalKernels/scan.h
+++ Sources/MetalKernels/scan.h
@@ -3,6 +3,11 @@
 #ifndef scan_h
 #define scan_h
 
+enum ScanBufferIndex {
+    ScanBufferIndexLength,
+    ScanBufferIndexIndirectArguments
+};
+
 #define SCAN_BLOCKSIZE        512
 
 #endif /* scan_h */
