8a19e7792e9efce8d9676ed9c3f6323ddbfbce24
Taylor Holliday <wtholliday@gmail.com>
2023-01-22 20:12:16 -0800

    Update GetMetalLibrary.m

--- Sources/MetalKernels/GetMetalLibrary.m
+++ Sources/MetalKernels/GetMetalLibrary.m
@@ -1,10 +1,16 @@
 //  Copyright © 2020 Audulus LLC. All rights reserved.
 
 #import "GetMetalLibrary.h"
+#import "ScanKernel.h"
 
 id<MTLLibrary> MetalKernelsGetMetalLibrary(id<MTLDevice> device) {
 
+    // If we're building an xcframework, we won't have SWIFTPM_MODULE_BUNDLE.
+#ifdef SWIFTPM_MODULE_BUNDLE
     NSBundle* bundle = SWIFTPM_MODULE_BUNDLE;
+#else
+    NSBundle* bundle = [NSBundle bundleForClass:ScanKernel.class];
+#endif
     assert(bundle);
 
     NSURL* libraryURL = [bundle URLForResource:@"default" withExtension:@"metallib"];
