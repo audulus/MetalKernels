ddd00c106efa34b5be70690dc3d6b48c16a10981
Taylor Holliday <wtholliday@gmail.com>
2023-05-04 15:28:19 -0700

    Delete xcframework.sh

--- xcframework.sh
+++
@@ -1,8 +0,0 @@
-# Copyright © 2023 Halfspace LLC. All rights reserved.
-
-# Script to build an xcframework from SculpturaCore so contractors can't access the core code.
-
-# run mint install unsignedapps/swift-create-xcframework
-# see https://github.com/unsignedapps/swift-create-xcframework
-
-swift create-xcframework --no-debug-symbols
