622ecbb42e9a9f2a435ef78f3b9e25f8595ca04c
Taylor Holliday <wtholliday@gmail.com>
2023-11-13 10:32:42 -0800

    Update SplitKernel.h

--- Sources/MetalKernels/include/SplitKernel.h
+++ Sources/MetalKernels/include/SplitKernel.h
@@ -4,9 +4,19 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/// Kernel for the split operation, which is a single
+/// step of a radix sort. int32 values are sorted by the i-th bit.
 @interface SplitKernel : NSObject
 
 - (instancetype)init:(id<MTLDevice>)device;
+
+/// Encode GPU commands for the split operation.
+/// - Parameters:
+///   - buffer: command buffer where commands are encoded
+///   - inputBuf: input buffer of int32s to be split
+///   - outputBuf: output buffer where int32s are written
+///   - bit: pivot bit (output will be sorted by this bit)
+///   - length: number of int32s to be split
 - (void) encodeSplitTo:(id<MTLCommandBuffer>)buffer
                  input:(id<MTLBuffer>)inputBuf
                 output:(id<MTLBuffer>)outputBuf
